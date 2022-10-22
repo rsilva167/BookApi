c1c82e4d6acd0408b791b46bb16eef36064843c7
Renan Santos <dosantosrenansilva@gmail.com>
2022-10-22 19:12:01 -0300

    Esta api simula um sistema de livros em uma biblioteca.

---
+++ BookAPI/Model/BookContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Model
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
